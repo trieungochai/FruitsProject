c737ee1a0a112bea70e21fb6ea7e6955dd8c5a45
Hai Trieu (BnK - HCM) <hai.trieu@bnksolution.com>
2021-09-21 15:00:50 +0700

    Introduce to Mongoose

--- app.js
+++ app.js
@@ -1,43 +1,64 @@
-const MongoClient = require("mongodb").MongoClient;
-const assert = require("assert");
+const mongoose = require("mongoose");
 
-// Connection URL
-const url = "mongodb://localhost:27017";
-
-// Database Name
-const dbName = "fruitsDB";
-
-// Use connect method to connect to the server
-MongoClient.connect(url, function (err, client) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  const db = client.db(dbName);
-
-  findDocuments(db, function () {
-    client.close();
-  });
+mongoose.connect("mongodb://localhost:27017/fruitsDB", {
+  useNewUrlParser: true,
 });
 
-const insertDocuments = function (db, callback) {
-  // Get the documents collection
-  const collection = db.collection("fruits");
-  // Insert some documents
-  collection.insertMany(
-    [
-      { name: "Apple", score: 8, review: "Great fruit" },
-      { name: "Orange", score: 6, review: "Kinda sour" },
-      { name: "Banana", score: 9, review: "Great stuff" },
-    ],
-    function (err, result) {
-      assert.equal(err, null);
-      assert.equal(3, result.result.n);
-      assert.equal(3, result.ops.length);
-      console.log("Inserted 3 documents into the collection");
-      callback(result);
-    }
-  );
-};
+const fruitSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  review: String,
+});
+
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+const fruit = new Fruit({
+  name: "Apple",
+  rating: 7,
+  review: "Pretty solid as a fruit.",
+});
+
+fruit.save();
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+  name: "John",
+  age: 37,
+});
+
+person.save();
+
+const kiwi = new Fruit({
+  name: "Kiwi",
+  score: 10,
+  review: "The best fruit!",
+});
+
+const orange = new Fruit({
+  name: "Orange",
+  rating: 4,
+  review: "Too sour for me",
+});
+
+const banana = new Fruit({
+  name: "Banana",
+  rating: 3,
+  review: "Weird texture",
+});
+
+Fruit.insertMany([kiwi, orange, banana], function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Successfully saved all the fruits to fruitsDB");
+  }
+});
 
 const findDocuments = function (db, callback) {
   // Get the documents collection
